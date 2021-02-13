a9f616c78a075280096bc45eac48ee53150f8e7b
= <=>
2021-02-13 07:17:05 +0530

    a

--- src/inject.js
+++ src/inject.js
@@ -26,14 +26,14 @@ function sendData(msg) {
         if (response && response.orderId) {
             WAPI.sendMessage2(msg.chatId._serialized, 'Order has been created, orderId: ' + response.orderId + ' for :' + response.shippingAddress);
         } else {
-            WAPI.sendMessage2(msg.chatId._serialized, 'It looks soemthing went wrong While crating order, Apologies for inconvinience \n' + msg.body, 'Quoted text');
+            WAPI.sendMessage2(msg.chatId._serialized, 'It looks soemthing went wrong While crating order, Apologies for inconvinience \n' + msg.body);
         }
         sendNextMsg();
 
     }).catch(function (error) {
         sendNextMsg();
         window.log(`Error===>${error.stack}`);
-        WAPI.sendMessage2(msg.chatId._serialized, 'It looks soemthing went wrong, Apologies for inconvinience \n' + msg.body);
+        WAPI.sendMessage2(msg.chatId._serialized, 'It looks soemthing went wrong, Apologies for inconvinience \n' + msg.body.shippingAddress);
     });
 }
 function pushMsg(msg) {
